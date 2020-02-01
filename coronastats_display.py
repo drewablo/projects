9d89ff970d5f6d42c92e6b4fe7bbfb8b0d84a87e
drewablo <andrewt77@gmail.com>
2020-01-31 18:00:09 -0600

    Update coronastats_display.py

--- coronastats_display.py
+++ coronastats_display.py
@@ -8,7 +8,11 @@ import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
 
-
+totalCasesPrevious = 0
+totalDeathsPrevious = 0
+UScasesPrevious = 0
+USopenTests = 0
+	
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
 dc_pin = digitalio.DigitalInOut(board.D25)
@@ -60,34 +64,48 @@ while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
     response = requests.get("https://www.worldometers.info/coronavirus/")
-    byte_data = response.content 
-    source_code = html.fromstring(byte_data) 
-    tree = source_code.xpath('//*[@id="maincounter-wrap"]/div/span')
-    tree2 = source_code.xpath('//*[@id="maincounter-wrap"][2]/div/span')
     response2 = requests.get("https://www.cdc.gov/coronavirus/2019-ncov/cases-in-us.html")
-    byte_data2 = response2.content 
+    
+    byte_data = response.content
+    byte_data2 = response2.content
+    
+    source_code = html.fromstring(byte_data) 
     source_code2 = html.fromstring(byte_data2) 
-    tree3 = source_code2.xpath('/html/body/div[6]/main/div[3]/div/div[3]/div[1]/div/div/div/div[2]/table/tbody/tr[1]/td')
-    tree4 = source_code2.xpath('/html/body/div[6]/main/div[3]/div/div[3]/div[1]/div/div/div/div[2]/table/tbody/tr[3]/td')
+    
+    totalCases = source_code.xpath('//*[@id="maincounter-wrap"]/div/span')
+    totalDeaths = source_code.xpath('//*[@id="maincounter-wrap"][2]/div/span')
+    UScases = source_code2.xpath('/html/body/div[6]/main/div[3]/div/div[3]/div[1]/div/div/div/div[2]/table/tbody/tr[1]/td')
+    USopenTests = source_code2.xpath('/html/body/div[6]/main/div[3]/div/div[3]/div[1]/div/div/div/div[2]/table/tbody/tr[3]/td')
+	
+	  TCdiff = str(totalCases - totalCasesPrevious)
+	  TDdiff = str(totalDeaths - totalDeathsPrevious)
+	  USCdiff = str(UScases - UScasesPrevious)
+	  USOdiff = str(USopenTests - USopenTestsPrevious)
+	
     update_time = time.localtime()
     t = time.asctime(update_time)
-    # Write four lines of text.
+	
     y = top+5
     draw.text((x, y), "CORONAVIRUS", font=font, fill="#FFFFFF")
     y += font.getsize("CORONAVIRUS")[1]
     draw.text((x, y), "TRACKER", font=font, fill="#FFFFFF")
     y += font.getsize("TRACKER")[1] + 10
-    draw.text((x, y), "Total: "+tree[0].text_content(), font=font, fill="#FFFF00")
-    y += font.getsize(tree[0].text_content())[1]
-    draw.text((x, y), "Dead: "+tree2[0].text_content(), font=font, fill="#FF0000")
-    y += font.getsize(tree2[0].text_content())[1]
-    draw.text((x, y), "US Cases: "+tree3[0].text_content(), font=font, fill="#FFa500")
-    y += font.getsize(tree3[0].text_content())[1]
-    draw.text((x, y), "US Open Tests: "+tree4[0].text_content(), font=font, fill="#00FF00")
-    y += font.getsize(tree4[0].text_content())[1] + 10
+    draw.text((x, y), "Total: "+totalCases[0].text_content()+" ("+TCdiff+")", font=font, fill="#FFFF00")
+    y += font.getsize(totalCases[0].text_content())[1]
+    draw.text((x, y), "Dead: "+totalDeaths[0].text_content()+" ("+TDdiff+")", font=font, fill="#FF0000")
+    y += font.getsize(totalDeaths[0].text_content())[1]
+    draw.text((x, y), "US Cases: "+UScases[0].text_content()+" ("+USCdiff+")", font=font, fill="#FFa500")
+    y += font.getsize(UScases[0].text_content())[1]
+    draw.text((x, y), "US Open Tests: "+USopenTests[0].text_content()+" ("+USOdiff+")", font=font, fill="#00FF00")
+    y += font.getsize(USopenTests[0].text_content())[1] + 10
     draw.text((x, y), "LAST UPDATED:", font=font, fill="#FFFFFF")
     y += font.getsize("LAST UPDATED:")[1]
     draw.text((x, y), t[:-4], font=font, fill="#FFFFFF")
     # Display image.
     disp.image(image, rotation)
-    time.sleep(5)
+	
+	  totalCasesPrevious = totalCases
+	  totalDeathsPrevious = totalDeaths
+	  UScasesPrevious = UScases
+	  USopenTestsPrevious = USopenTests
+    time.sleep(60)
