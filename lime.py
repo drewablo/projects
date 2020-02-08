229eade2d5f0d94b6f7635a84f9d067eaf8a51e2
drewablo <andrewt77@gmail.com>
2020-02-07 18:43:11 -0600

    Update lime.py

--- lime.py
+++ lime.py
@@ -78,6 +78,21 @@ font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 24)
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
+class Timer:
+	def __init__(self,timer_period):
+		self.timer_period = timer_period
+		self.update_timer()
+	def update_timer(self):
+		self.last_time = time()*100
+		self.timer_expires = self.last_time + self.timer_period
+	def has_timer_expired(self):
+		if time()*100 > self.timer_expires:
+			self.update_timer()
+			return 1
+		else:
+			return 0
+
+timer = Timer(500)
 
 def symbolUpdate(caseChange):
 	global previousSymbol
@@ -95,60 +110,58 @@ def coronoaStats():
 	global totalCoronaDeathsPrevious
 	global UScasesPrevious
 	global USopenTestsPrevious
-	
-	response = requests.get("https://www.worldometers.info/coronavirus/")
-	response2 = requests.get("https://www.cdc.gov/coronavirus/2019-ncov/cases-in-us.html")
+	if timer.has_timer_expired():
+		response = requests.get("https://www.worldometers.info/coronavirus/")
+		response2 = requests.get("https://www.cdc.gov/coronavirus/2019-ncov/cases-in-us.html")
 
-	byte_data = response.content
-	byte_data2 = response2.content
+		byte_data = response.content
+		byte_data2 = response2.content
 
-	source_code = html.fromstring(byte_data) 
-	source_code2 = html.fromstring(byte_data2) 
+		source_code = html.fromstring(byte_data) 
+		source_code2 = html.fromstring(byte_data2) 
 
-	totalCases = source_code.xpath('//*[@id="maincounter-wrap"]/div/span')
-	totalDeaths = source_code.xpath('//*[@id="maincounter-wrap"][2]/div/span')
+		totalCases = source_code.xpath('//*[@id="maincounter-wrap"]/div/span')
+		totalDeaths = source_code.xpath('//*[@id="maincounter-wrap"][2]/div/span')
 
-	UScases = source_code2.xpath('/html/body/div[6]/main/div[3]/div/div[3]/div[1]/div/div/div/div[2]/table/tbody/tr[1]/td')
-	USopenTests = source_code2.xpath('/html/body/div[6]/main/div[3]/div/div[3]/div[1]/div/div/div/div[2]/table/tbody/tr[3]/td')
+		UScases = source_code2.xpath('/html/body/div[6]/main/div[3]/div/div[3]/div[1]/div/div/div/div[2]/table/tbody/tr[1]/td')
+		USopenTests = source_code2.xpath('/html/body/div[6]/main/div[3]/div/div[3]/div[1]/div/div/div/div[2]/table/tbody/tr[3]/td')
 
-	totalCasesReturn = totalCases[0].text_content()
-	totalDeathsReturn = totalDeaths[0].text_content()
-	UScasesReturn = UScases[0].text_content()
-	USopenTestsReturn = USopenTests[0].text_content()
+		totalCasesReturn = totalCases[0].text_content()
+		totalDeathsReturn = totalDeaths[0].text_content()
+		UScasesReturn = UScases[0].text_content()
+		USopenTestsReturn = USopenTests[0].text_content()
 
 
-	totalCaseChange = int(re.findall("\d+",totalCasesReturn)[0]) - totalCoronaCasesPrevious
-	totalDeathChange = int(re.findall("\d+",totalDeathsReturn)[0]) - totalCoronaDeathsPrevious
-	UScasesChange = int(re.findall("\d+",UScasesReturn)[0]) - UScasesPrevious
-	USopenChange = int(re.findall("\d+",USopenTestsReturn)[0]) - USopenTestsPrevious
+		totalCaseChange = int(re.findall("\d+",totalCasesReturn)[0]) - totalCoronaCasesPrevious
+		totalDeathChange = int(re.findall("\d+",totalDeathsReturn)[0]) - totalCoronaDeathsPrevious
+		UScasesChange = int(re.findall("\d+",UScasesReturn)[0]) - UScasesPrevious
+		USopenChange = int(re.findall("\d+",USopenTestsReturn)[0]) - USopenTestsPrevious
 
-	update_time = time.localtime()
-	t = time.asctime(update_time)
+		update_time = time.localtime()
+		t = time.asctime(update_time)
 
-	if totalCaseChange != 0 or totalDeathChange != 0 or UScasesChange !=0 or USopenChange !=0:
-		y = top+5
-		draw.text((x, y), "CORONAVIRUS", font=font, fill="#FFFFFF")
-		y += font.getsize("CORONAVIRUS")[1] + 10
-		draw.text((x, y), symbolUpdate(totalCaseChange) + " Total: " + totalCasesReturn, font=font, fill="#FFFF00")
-		y += font.getsize(totalCasesReturn)[1]
-		draw.text((x, y), symbolUpdate(totalDeathChange)+  " Dead: " + totalDeathsReturn, font=font, fill="#FF0000")
-		y += font.getsize(totalDeathsReturn)[1]
-		draw.text((x, y), symbolUpdate(UScasesChange) + " US Cases: " + UScasesReturn, font=font, fill="#FFa500")
-		y += font.getsize(UScasesReturn)[1]
-		draw.text((x, y), symbolUpdate(USopenChange) + " US Tests: " + USopenTestsReturn, font=font, fill="#00FF00")
-		y += font.getsize(USopenTestsReturn)[1] + 10
-		draw.text((x, y), "LAST CHANGE:", font=font, fill="#FFFFFF")
-		y += font.getsize("LAST CHANGE:")[1]
-		draw.text((x, y), t[:-4], font=font, fill="#FFFFFF")
-		# Display image.
-		disp.image(image, rotation)
+		if totalCaseChange != 0 or totalDeathChange != 0 or UScasesChange !=0 or USopenChange !=0:
+			y = top+5
+			draw.text((x, y), "CORONAVIRUS", font=font, fill="#FFFFFF")
+			y += font.getsize("CORONAVIRUS")[1] + 10
+			draw.text((x, y), symbolUpdate(totalCaseChange) + " Total: " + totalCasesReturn, font=font, fill="#FFFF00")
+			y += font.getsize(totalCasesReturn)[1]
+			draw.text((x, y), symbolUpdate(totalDeathChange)+  " Dead: " + totalDeathsReturn, font=font, fill="#FF0000")
+			y += font.getsize(totalDeathsReturn)[1]
+			draw.text((x, y), symbolUpdate(UScasesChange) + " US Cases: " + UScasesReturn, font=font, fill="#FFa500")
+			y += font.getsize(UScasesReturn)[1]
+			draw.text((x, y), symbolUpdate(USopenChange) + " US Tests: " + USopenTestsReturn, font=font, fill="#00FF00")
+			y += font.getsize(USopenTestsReturn)[1] + 10
+			draw.text((x, y), "LAST CHANGE:", font=font, fill="#FFFFFF")
+			y += font.getsize("LAST CHANGE:")[1]
+			draw.text((x, y), t[:-4], font=font, fill="#FFFFFF")
+			# Display image.
+			disp.image(image, rotation)
 
-	totalCoronaCasesPrevious = int(re.findall("\d+",totalCasesReturn)[0])
-	totalCoronaDeathsPrevious = int(re.findall("\d+",totalDeathsReturn)[0])
-	UScasesPrevious = int(re.findall("\d+",UScasesReturn)[0])
-	USopenTestsPrevious = int(re.findall("\d+",USopenTestsReturn)[0])
-
-	time.sleep(.1)
+		totalCoronaCasesPrevious = int(re.findall("\d+",totalCasesReturn)[0])
+		totalCoronaDeathsPrevious = int(re.findall("\d+",totalDeathsReturn)[0])
+		UScasesPrevious = int(re.findall("\d+",UScasesReturn)[0])
+		USopenTestsPrevious = int(re.findall("\d+",USopenTestsReturn)[0])
 
 def averageNumbers(xPathContent):
 	numbersList = re.findall("(?:^|\s)(\d*\.?\d+|\d{1,3}(?:,\d{3})*(?:\.\d+)?)(?!\S)",xPathContent)
@@ -163,52 +176,51 @@ def fluStats():
 	global totalFluCasesPrevious
 	global totalFlueCasesPrevious
 	global totalHospitalizationsPrev
+	if timer.has_timer_expired():
+		response = requests.get("https://www.cdc.gov/flu/about/burden/preliminary-in-season-estimates.htm")
+		byte_data = response.content 
+		source_code = html.fromstring(byte_data) 
 
-	response = requests.get("https://www.cdc.gov/flu/about/burden/preliminary-in-season-estimates.htm")
-	byte_data = response.content 
-	source_code = html.fromstring(byte_data) 
+		rawSick = source_code.xpath("/html/body/div[6]/main/div[3]/div/div[4]/div[2]/div[1]/div/div/h4/strong[1]")
+		rawHospital = source_code.xpath("/html/body/div[6]/main/div[3]/div/div[4]/div[3]/div[1]/div/div/h4/strong[1]")
+		rawDeaths = source_code.xpath("/html/body/div[6]/main/div[3]/div/div[4]/div[3]/div[2]/div/div/h4/strong[1]")
 
-	rawSick = source_code.xpath("/html/body/div[6]/main/div[3]/div/div[4]/div[2]/div[1]/div/div/h4/strong[1]")
-	rawHospital = source_code.xpath("/html/body/div[6]/main/div[3]/div/div[4]/div[3]/div[1]/div/div/h4/strong[1]")
-	rawDeaths = source_code.xpath("/html/body/div[6]/main/div[3]/div/div[4]/div[3]/div[2]/div/div/h4/strong[1]")
+		rawSickReturn = rawSick[0].text_content()
+		rawHospitalReturn = rawHospital[0].text_content()
+		rawDeathsReturn = rawDeaths[0].text_content()
 
-	rawSickReturn = rawSick[0].text_content()
-	rawHospitalReturn = rawHospital[0].text_content()
-	rawDeathsReturn = rawDeaths[0].text_content()
+		update_time = time.localtime()
+		t = time.asctime(update_time)
 
-	update_time = time.localtime()
-	t = time.asctime(update_time)
+		totalCaseChange = averageNumbers(rawSickReturn)- totalFluCasesPrevious
+		totalDeathChange = averageNumbers(rawHospitalReturn) - totalFlueCasesPrevious
+		totalHospitalizationsChange = averageNumbers(rawDeathsReturn)- totalHospitalizationsPrev
 
-	totalCaseChange = averageNumbers(rawSickReturn)- totalFluCasesPrevious
-	totalDeathChange = averageNumbers(rawHospitalReturn) - totalFlueCasesPrevious
-	totalHospitalizationsChange = averageNumbers(rawDeathsReturn)- totalHospitalizationsPrev
+		if totalCaseChange != 0 or totalDeathChange != 0 or totalHospitalizationsChange !=0:
+			y = top+5
+			draw.text((x, y), "INFLUENZA USA", font=font, fill="#FFFFFF")
+			y += font.getsize("INFLUENZA USA")[1] + 4
+			draw.text((x, y), "Infections: ", font=font, fill="#FFFF00")
+			y += font.getsize("Infections: ")[1]
+			draw.text((x, y), str(f'{averageNumbers(rawSickReturn):n}'), font=font, fill="#FFFF00")
+			y += font.getsize(str(f'{averageNumbers(rawSickReturn):n}'))[1]+2
+			draw.text((x, y), "Deaths: ", font=font, fill="#FF0000")
+			y += font.getsize("Deaths: ")[1]
+			draw.text((x, y), str(f'{averageNumbers(rawDeathsReturn):n}'), font=font, fill="#FF0000")
+			y += font.getsize(str(f'{averageNumbers(rawDeathsReturn):n}'))[1]+2		
+			draw.text((x, y), "Hospitalizations: ", font=font, fill="#FFa500")
+			y += font.getsize("Hospitalizations: ")[1]
+			draw.text((x, y), str(f'{averageNumbers(rawHospitalReturn):n}'), font=font, fill="#FFa500")
+			y += font.getsize(str(f'{averageNumbers(rawHospitalReturn):n}'))[1]+4				
+			draw.text((x, y), "LAST CHANGE:", font=font, fill="#FFFFFF")
+			y += font.getsize("LAST CHANGE:")[1]
+			draw.text((x, y), t[:-4], font=font, fill="#FFFFFF")
+			# Display image.
+			disp.image(image, rotation)
 
-	if totalCaseChange != 0 or totalDeathChange != 0 or totalHospitalizationsChange !=0:
-		y = top+5
-		draw.text((x, y), "INFLUENZA USA", font=font, fill="#FFFFFF")
-		y += font.getsize("INFLUENZA USA")[1] + 4
-		draw.text((x, y), "Infections: ", font=font, fill="#FFFF00")
-		y += font.getsize("Infections: ")[1]
-		draw.text((x, y), str(f'{averageNumbers(rawSickReturn):n}'), font=font, fill="#FFFF00")
-		y += font.getsize(str(f'{averageNumbers(rawSickReturn):n}'))[1]+2
-		draw.text((x, y), "Deaths: ", font=font, fill="#FF0000")
-		y += font.getsize("Deaths: ")[1]
-		draw.text((x, y), str(f'{averageNumbers(rawDeathsReturn):n}'), font=font, fill="#FF0000")
-		y += font.getsize(str(f'{averageNumbers(rawDeathsReturn):n}'))[1]+2		
-		draw.text((x, y), "Hospitalizations: ", font=font, fill="#FFa500")
-		y += font.getsize("Hospitalizations: ")[1]
-		draw.text((x, y), str(f'{averageNumbers(rawHospitalReturn):n}'), font=font, fill="#FFa500")
-		y += font.getsize(str(f'{averageNumbers(rawHospitalReturn):n}'))[1]+4				
-		draw.text((x, y), "LAST CHANGE:", font=font, fill="#FFFFFF")
-		y += font.getsize("LAST CHANGE:")[1]
-		draw.text((x, y), t[:-4], font=font, fill="#FFFFFF")
-		# Display image.
-		disp.image(image, rotation)
-
-	totalFluCasesPrevious = averageNumbers(rawSickReturn)
-	totalFlueCasesPrevious = averageNumbers(rawHospitalReturn)
-	totalHospitalizationsPrev = averageNumbers(rawDeathsReturn)
-	time.sleep(.1)
+		totalFluCasesPrevious = averageNumbers(rawSickReturn)
+		totalFlueCasesPrevious = averageNumbers(rawHospitalReturn)
+		totalHospitalizationsPrev = averageNumbers(rawDeathsReturn)
 
 while True:
 	if switcher.is_pressed:
