d5a22c2711e12bce03e025a872f9e718f3260750
drewablo <andrewt77@gmail.com>
2020-02-08 09:34:14 -0600

    Update lime.py

--- lime.py
+++ lime.py
@@ -224,12 +224,12 @@ def buttonPress():
 	disp.image(image)
 	print("Button Pressed: " + str(screenState))
 	if screenState == True:
-		coronoaStats()
+		fluStats()
 		screenState = False
 		print(screenState)
 		return screenState		
 	elif screenState == False:
-		fluStats()
+		coronoaStats()
 		screenState = True
 		print(screenState)
 		return screenState
